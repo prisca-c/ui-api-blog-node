d5857dbaedba181c821f2479b4e9ed2bca254f7d
Prisca <prisca.castanier@gmail.com>
2022-11-24 14:40:14 +0100

    added api_blog.js to handle api call

---
+++ src/api/api_blog.js
@@ -0,0 +1,28 @@
+export const getCategories = () => {
+  return fetch('http://localhost:8080/category', {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((err) => console.log(err));
+}
+
+export const getArticles = (category) => {
+  return fetch(`http://localhost:8080/category/${category}`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((err) => console.log(err));
+}
+
+
+
