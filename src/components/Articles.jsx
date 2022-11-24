352494b648daeb10e5baada826ec565303d433af
Prisca <prisca.castanier@gmail.com>
2022-11-24 14:42:19 +0100

    articles are now displayed

---
+++ src/components/Articles.jsx
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from "react";
+import {getArticles} from "../api/api_blog";
+
+const Articles = (props) => {
+  const [articlesState, setArticlesState] = useState([])
+
+  const fetchArticles = async () => {
+    getArticles(props.category).then((response) => {
+      setArticlesState(response)
+    })
+  }
+
+  useEffect(() => {
+    fetchArticles()
+  },[articlesState])
+
+  return (
+    <div className={"articles"}>
+      <h3>Articles</h3>
+      {
+        articlesState.map((article) => {
+          return (
+            <div key={article}>
+              <p onClick={props.handleClick} className={"article"}>{article}</p>
+            </div>
+          )
+        })
+      }
+    </div>
+  );
+}
+
+export default Articles;
