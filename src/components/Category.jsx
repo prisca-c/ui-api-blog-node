b19f276e1b1d051f6950a59e57833fc1fff03ab1
Prisca <prisca.castanier@gmail.com>
2022-11-24 14:42:10 +0100

    categories are now displayed

---
+++ src/components/Category.jsx
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from "react";
+import {getCategories} from "../api/api_blog";
+
+const Category = (props) => {
+  const [categoriesState, setCategoriesState] = useState([])
+
+  useEffect(() => {
+    const fetchCategory = async () => {
+      try{
+        getCategories().then((response) => {
+          setCategoriesState(response)
+        })
+      }
+      catch (e) {
+        console.log(e)
+      }
+    }
+    fetchCategory()
+  },[categoriesState])
+
+  return (
+    <div className={"categories"}>
+      <h3>Categories</h3>
+      {
+        categoriesState.map((category) => {
+          return (
+            <div key={category}>
+              <p className={"category"} onClick={props.handleClick}>{category}</p>
+            </div>
+          )
+        })
+      }
+    </div>
+  );
+}
+
+export default Category;
